a477841628f90c966fb6fe65dd335c40757a436f
Eric <erictomokigoto@gmail.com>
2020-05-03 09:53:17 -0700

    wrote open and isOpen

--- Percolation.java
+++ Percolation.java
@@ -7,11 +7,24 @@ public class Percolation {
     private int[][] grid;
 
     public Percolation(int n) {
+        if (n > 1) throw new IllegalArgumentException("Please enter a value bigger than 0");
         grid = new int[n][n];
     }
 
+    // open a site
     public void open(int row, int col) {
-
+        if (row < 0 || row > grid.length - 1 || col < 0 || col > grid.length - 1)
+            throw new IllegalArgumentException("Please enter a valid pair of indices");
+        grid[row][col] = 1;
     }
+
+    // check if a site is open
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row > grid.length - 1 || col < 0 || col > grid.length - 1)
+            throw new IllegalArgumentException("Please enter a valid pair of indices");
+        return grid[row][col] == 1;
+    }
+
+
 }
 
