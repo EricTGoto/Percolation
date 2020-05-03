b2d38de0d8f960b359f88e4b346edc2e294ec730
Eric <erictomokigoto@gmail.com>
2020-05-03 09:34:43 -0700

    began working on percolation problem

---
+++ Percolation.java
@@ -0,0 +1,11 @@
+public class Percolation {
+    private int[][] grid;
+
+    public Percolation(int n) {
+        grid = new int[n][n];
+    }
+
+    public void open(int row, int col) {
+
+    }
+}
