c4cce679eaccf538643772de10b316bafde7c148
Eric <erictomokigoto@gmail.com>
2020-05-03 09:47:33 -0700

    added comments

--- Percolation.java
+++ Percolation.java
@@ -1,3 +1,8 @@
+// the percolating system will be modelled by an n by n grid of sites. 0 for blocked and 1 for open.
+// A system "percolates" when there is a chain of sites starting from the top row to the bottom row
+// This idea of percolation is a useful idea in many physical applications such as semiconductor manufacturing,
+// water drainage, social media connections and so on
+
 public class Percolation {
     private int[][] grid;
 
@@ -9,3 +14,4 @@ public class Percolation {
 
     }
 }
+
